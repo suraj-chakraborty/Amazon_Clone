1e77c71f719bbc3966e31f680d92ba32b7883f7a
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-03-22 12:42:22 +0000

    Banner component

---
+++ src/component/ProductFeed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductFeed() {
+  return <div>Productfeed</div>;
+}
+
+export default ProductFeed;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import Banner from "../component/Banner";
 import Header from "../component/Header";
+import ProductFeed from "../component/Productfeed";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title> Amazon 2.0 </title>
       </Head>
@@ -11,6 +13,12 @@ export default function Home() {
       {/* <h1>Amazon</h1> */}
       {/* HEADER */}
       <Header />
+      <main className="max-w-screen-xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Productfeed */}
+        <ProductFeed />
+      </main>
     </div>
   );
 }
