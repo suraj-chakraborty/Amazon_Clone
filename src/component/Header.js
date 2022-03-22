aaed000ab6ead358fed223566ab721e74e0bf58f
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-03-22 12:11:42 +0000

    Header completed

--- src/component/Header.js
+++ src/component/Header.js
@@ -18,28 +18,49 @@ function Header() {
         {/* search */}
         <div className="hidden sm:flex items-center h-10 rounded-md cursor-pointer flex-grow bg-yellow-400 hover:bg-yellow-500 ">
           <input
-            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md outline-none"
             type="text"
           />
           <SearchIcon className="h-12 p-4" />
         </div>
         {/* Right */}
-        <div className="text-white flex items-center text-xs space-x-5 mx-6 whitespace-nowrap cursor-pointer">
-          <div>
+        <div className="text-white flex items-center text-xs space-x-5 mx-6 whitespace-nowrap ">
+          <div className="link">
             <p>Hello world</p>
-            <p>Account & List</p>
+            <p className="font-extrabold md:text-sm">Account & List</p>
           </div>
-          <div>
-            <p>Returns & Orders</p>
+          <div className="link text-center">
+            <p className="font-extrabold md:text-sm">
+              Returns<br></br> & Orders
+            </p>
           </div>
-          <div>
-            <ShoppingCartIcon />
-            <p>Cart</p>
+          <div className=" relative link flex items-center">
+            <span className="absolute top-0 right-0 lg:right-0  md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden  md:inline font-extrabold md:text-sm mt-2">
+              Cart
+            </p>
           </div>
         </div>
       </div>
       {/* bottom navigation */}
-      <div></div>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">prime video</p>
+        <p className="link">Amazon business</p>
+        <p className="link">Today's Deal</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopping Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 }
