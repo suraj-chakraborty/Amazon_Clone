6840255f4c8d6d91b0b12f5e0b626b459af66282
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-03-28 22:50:54 +0100

    payment with stripe

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -6,11 +6,30 @@ import Header from "../component/Header";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const session = useSession();
   const total = useSelector(selectTotal);
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    // call the backend to create checkout session
+    const checkoutSession = await axios.post("api/create-checkout-session", {
+      items: items,
+      email: session.data.user.email,
+    });
+    // redirect user
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) {
+      alert(result.error.message);
+    }
+  };
   return (
     <div className="bg-gray-100">
       <Header />
@@ -57,6 +76,8 @@ function Checkout() {
                 </span>
               </h2>
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
